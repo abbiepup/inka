80d22d47bd5c54003917a6a01fcf2929a85e7568
KiraCoding <38864051+KiraCoding@users.noreply.github.com>
2024-08-26 21:59:13 +0200

    wip

--- src/base.rs
+++ src/base.rs
@@ -1,7 +1,9 @@
+use core::fmt::{Formatter, Pointer, Result};
 use core::ptr::NonNull;
+use std::fmt::Debug;
 
 /// Thread-safe `Base` pointer
-#[derive(Debug, Clone, Copy)]
+#[derive(Clone, Copy)]
 pub struct Base {
     pub(crate) ptr: NonNull<u8>,
 }
@@ -10,6 +12,18 @@ impl Base {
     #[inline]
     pub fn as_ptr(&self) -> *const u8 {
         self.ptr.as_ptr()
+    }
+}
+
+impl Debug for Base {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        Pointer::fmt(&self.ptr, f)
+    }
+}
+
+impl Pointer for Base {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        Pointer::fmt(&self.ptr, f)
     }
 }
 
