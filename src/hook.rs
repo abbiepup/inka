3d1ac81623eb2f8706df1f917992178f7a2e6982
KiraCoding <38864051+KiraCoding@users.noreply.github.com>
2024-08-27 23:27:27 +0200

    wip

--- src/hook.rs
+++ src/hook.rs
@@ -1,4 +1,14 @@
+use core::sync::atomic::{AtomicPtr, Ordering};
+use core::ptr::null_mut;
+
 pub trait Hook<F>: Copy {
+
+    fn hook(&self) -> HookGuard {
+        let self_ptr = self.as_ptr_u8();
+
+        HookGuard { bytes: [0; 16] }
+    }
+
     fn as_ptr_u8(self) -> *mut u8;
     fn trampoline(f: F) -> Closure<F>;
 }
@@ -11,9 +21,8 @@ impl HookGuard {
     pub fn unhook(&self) {}
 }
 
-
 pub struct Closure<F> {
-    inner: Box<ClosureInner<F>>
+    inner: Box<ClosureInner<F>>,
 }
 
 #[repr(C)]
@@ -22,3 +31,30 @@ struct ClosureInner<F> {
     data: F,
 }
 
+static STATIC_CONTEXT: AtomicPtr<()> = AtomicPtr::new(null_mut());
+
+impl<F, R> Hook<F> for unsafe extern "C" fn() -> R
+where
+    F: FnMut() + 'static,
+{
+    fn as_ptr_u8(self) -> *mut u8 {
+        self as *mut u8
+    }
+
+    fn trampoline(f: F) -> Closure<F> {
+        unsafe extern "C" fn thunk<F, R>()
+        where
+            F: FnMut(),
+        {
+            let p = STATIC_CONTEXT.swap(null_mut(), Ordering::Relaxed) as *mut ClosureInner<F>;
+            ((*p).data)();
+        }
+
+        Closure {
+            inner: Box::new(ClosureInner {
+                ptr: thunk::<F, R> as *const (),
+                data: f,
+            }),
+        }
+    }
+}
